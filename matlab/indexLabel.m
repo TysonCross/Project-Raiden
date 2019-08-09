ff63b9fb4117cd7650242413b43df60280059e68
JasonSmitR <jason.smit@students.wits.ac.za>
2019-08-09 15:58:56 +0200

    findLightingEvents and findStrokeEvents are now functional

---
+++ matlab/indexLabel.m
@@ -0,0 +1,20 @@
+%    outputDs = imageDatastore('/home/jason/Desktop/test/output', 'FileExtensions' , '.png');
+function labelList = indexLabel(outputDs,labelID,opt)
+    numImages=numel(outputDs.Files);
+    occuranceFrames=zeros(1,numImages);
+    for i=1 : numImages 
+        tmp = outputDs.readimage(i);
+        if any(tmp(:) == labelID) % any frame has the event, do
+            occuranceFrames(i) = 1; % true
+        end
+        %else load new image
+    end
+    if nargin == 2
+           labelList = find(occuranceFrames);
+       
+    elseif(opt == 'logical')
+          labelList = occuranceFrames;
+    else
+        error('Invalid option');
+    end
+end
